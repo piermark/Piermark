fda8db78516bdbdc3fb1b6dd2895e1902250084e
piermark <piermark@gmail.com>
2025-06-05 15:12:38 +0200

    Fix dial string for call forwarding

--- Reperibilita/app/src/main/java/com/example/reperibilita/ForwardingService.kt
+++ Reperibilita/app/src/main/java/com/example/reperibilita/ForwardingService.kt
@@ -41,7 +41,7 @@ class ForwardingService : Service() {
     }
 
     private fun sendActivationCode(phoneNumber: String) {
-        val ussd = "*21*+39${phoneNumber}#"
+        val ussd = "*21*${phoneNumber}#"
         val uri = Uri.parse("tel:" + Uri.encode(ussd, "#"))
         val intent = Intent(Intent.ACTION_CALL, uri)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
